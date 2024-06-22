4f8e37d98f73a3323622a18d1b7efacbf5d33e08
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2024-06-22 10:45:42 +0530

    added all the ui clearly

--- client/src/components/quiz/TestResult.jsx
+++ client/src/components/quiz/TestResult.jsx
@@ -57,19 +57,19 @@ const TestResult = () => {
   };
 
   return (
-    <div className="h-screen flex flex-col items-center justify-center font-poppins">
-      <div className="bg-white h-[8%] w-[90%] top-0 absolute mt-3 mb-2 rounded-lg"></div>
+    <div className="min-h-screen flex flex-col items-center justify-center font-poppins">
+      <div className="w-[90%]  h-[10%] bg-white"></div>
 
-      <div className=" absolute top-[15%] flex flex-col justify-center items-center">
-        <h1 className="text-3xl font-bold  text-green-600">
+      <div className="flex flex-col justify-center items-center w-full px-4 md:w-[80%] lg:w-[60%] xl:w-[50%]">
+        <h1 className="md:text-3xl text-xl font-bold text-green-600 mt-[10%]">
           Thank you for taking the test
         </h1>
-        <h1 className="text-3xl font-bold mb-4 md:mt-[1rem] text-green-600">
+        <h1 className="md:text-3xl text-xl  font-bold mb-4 md:mt-4 mt-[10%] text-green-600">
           Your Report
         </h1>
-        <div className="flex flex-col   md:w-[150%] md:flex-row justify-around mt-10">
-          <div className="md:w-1/3 rounded-xl bg-white p-4 mt-5">
-            <h2 className="text-xl font-bold mb-4 text-nowrap">
+        <div className="flex flex-col md:flex-row justify-around w-full mt-8">
+          <div className="md:w-[45%] m-10 md:m-1 rounded-xl bg-white p-4 mb-8 md:mb-0 relative">
+            <h2 className="text-xl font-bold mb-4 text-center">
               Questions Attended
             </h2>
             <div className="flex justify-center">
@@ -80,59 +80,57 @@ const TestResult = () => {
                 "#DC6C2D"
               )}
             </div>
-            <div className="flex flex-col justify-start mt-4">
-              <div className="flex flex-row justify-start items-center">
-                <div className="bg-[#DC6C2D] w-3 h-3 mr-2"></div>
-                <p>Questions Attended: {wrongCount + correctCount}</p>
+            <div className="flex flex-col mt-4">
+              <div className="flex items-center justify-between">
+                <div>
+                  <p>Questions Attended: {wrongCount + correctCount}</p>
+                </div>
+                <div className="flex items-center">
+                  <div className="bg-[#DC6C2D] w-3 h-3 ml-2 justify-end"></div>
+                </div>
               </div>
               <hr className="w-full my-4 border-t-2 border-gray-400" />
-              <div className="flex flex-row justify-stat items-center">
-                <div className="bg-[#777777] w-3 h-3 mr-2"></div>
-                <div className="justify-end">
-                  <p>Total Questions: {totalQuestions}</p>
-                </div>
+              <div className="flex items-center justify-between">
+                <p>Total Questions: {totalQuestions}</p>
+                <div className="bg-[#777777] w-3 h-3 ml-2"></div>
               </div>
             </div>
           </div>
-          <div className="w-full rounded-xl md:w-1/3  bg-white mt-5 p-4">
-            <h2 className="text-xl font-bold mb-4">Correct Answers</h2>
+          <div className="md:w-[45%] m-10 md:m-1 rounded-xl bg-white p-4 relative">
+            <h2 className="text-xl font-bold text-center mb-4">
+              Correct Answers
+            </h2>
             <div className="flex justify-center">
               {getDoughnutChart(
                 correctCount,
                 totalQuestions,
                 "#1B9D34",
-
                 "#1B9D34"
               )}
             </div>
-            <div className="text-center mt-4">
-              <div className="flex flex-row justify-start items-center">
-                <div className="bg-[#1B9D34] w-3 mr-2 h-3"></div>
+            <div className="flex flex-col mt-4">
+              <div className="flex items-center justify-between">
                 <p>Correctly Answered: {correctCount}</p>
+                <div className="bg-[#1B9D34] w-3 h-3 ml-2"></div>
               </div>
               <hr className="w-full my-4 border-t-2 border-gray-400" />
-              <div className="flex flex-row justify-start items-center">
-                <div className="bg-[#777777] w-3 mr-2 h-3"></div>
-                <div className="justify-end">
-                  <p>Total Questions: {totalQuestions}</p>
-                </div>{" "}
+              <div className="flex items-center justify-between">
+                <p>Total Questions: {totalQuestions}</p>
+                <div className="bg-[#777777] w-3 h-3 ml-2"></div>
               </div>
             </div>
           </div>
         </div>
-        {/* <p className="mt-7 font-semibold">
-          Average Time Per Question:
-          <span className="font-bold"> {averageTimePerQuestion} seconds</span>
-        </p> */}
+
         <button
-          className="m-3 bg-ind hover:bg-violet-700 text-white font-bold py-2 px-4 rounded"
+          className="mt-8 bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
           onClick={handleBackToHome}
         >
           Back to Home
         </button>
       </div>
 
-      <div className="bg-white h-[8%] absolute bottom-0  w-[90%] mb-6 rounded-lg"></div>
+      <div className="w-[90%] h-[10%] bg-white"></div>
     </div>
   );
 };
