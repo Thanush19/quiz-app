e269997745540b2c116476e5e517305a16fb1eac
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2024-06-22 11:12:15 +0530

    ui correction in test result

--- client/src/components/quiz/TestResult.jsx
+++ client/src/components/quiz/TestResult.jsx
@@ -58,18 +58,18 @@ const TestResult = () => {
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center font-poppins">
-      <div className="w-[90%]  h-[10%] bg-white"></div>
+      <div className="w-[90%] md:h-14 h-10 mt-3 rounded-3xl bg-white"></div>
 
-      <div className="flex flex-col justify-center items-center w-full px-4 md:w-[80%] lg:w-[60%] xl:w-[50%]">
-        <h1 className="md:text-3xl text-xl font-bold text-green-600 mt-[10%]">
+      <div className="flex flex-col justify-center items-center w-full px-4 md:w-[80%] lg:w-[60%] xl:w-[50%] min-h-[calc(100vh-8rem)]">
+        <h1 className="md:text-3xl text-md font-bold text-green-600 mt-4">
           Thank you for taking the test
         </h1>
-        <h1 className="md:text-3xl text-xl  font-bold mb-4 md:mt-4 mt-[10%] text-green-600">
+        <h1 className="md:text-3xl text-md font-bold mb-4 md:mt-4 mt-4 text-green-600">
           Your Report
         </h1>
-        <div className="flex flex-col md:flex-row justify-around w-full mt-8">
-          <div className="md:w-[45%] m-10 md:m-1 rounded-xl bg-white p-4 mb-8 md:mb-0 relative">
-            <h2 className="text-xl font-bold mb-4 text-center">
+        <div className="flex flex-col md:flex-row justify-around w-full md:mt-8">
+          <div className="md:w-[45%] m-7 md:m-1 rounded-xl bg-white p-4 mb-8 md:mb-0 relative">
+            <h2 className="md:first-line:text-xl text-[1px] text-nowrap font-bold mb-4 text-center">
               Questions Attended
             </h2>
             <div className="flex justify-center">
@@ -80,23 +80,32 @@ const TestResult = () => {
                 "#DC6C2D"
               )}
             </div>
-            <div className="flex flex-col mt-4">
-              <div className="flex items-center justify-between">
-                <div>
-                  <p>Questions Attended: {wrongCount + correctCount}</p>
-                </div>
+            <div className="flex flex-col mt-4  font-medium ">
+              <div className="flex items-center font-medium justify-between">
                 <div className="flex items-center">
                   <div className="bg-[#DC6C2D] w-3 h-3 ml-2 justify-end"></div>
                 </div>
+                <div className=" font-medium md:text-lg text-sm  ">
+                  Questions Attended
+                </div>
+                <div className=" font-medium md:text-lg text-sm  ">
+                  {wrongCount + correctCount}
+                </div>
               </div>
-              <hr className="w-full my-4 border-t-2 border-gray-400" />
+              <hr className="w-full my-4 border-t-2 border-[#414142]" />
               <div className="flex items-center justify-between">
-                <p>Total Questions: {totalQuestions}</p>
                 <div className="bg-[#777777] w-3 h-3 ml-2"></div>
+                <div className=" font-medium  md:text-lg text-sm  ">
+                  {" "}
+                  Total Questions
+                </div>
+                <div className=" font-medium md:text-lg text-sm ">
+                  {totalQuestions}
+                </div>
               </div>
             </div>
           </div>
-          <div className="md:w-[45%] m-10 md:m-1 rounded-xl bg-white p-4 relative">
+          <div className="md:w-[45%] m-7 md:m-1 rounded-xl bg-white p-4 relative">
             <h2 className="text-xl font-bold text-center mb-4">
               Correct Answers
             </h2>
@@ -110,27 +119,39 @@ const TestResult = () => {
             </div>
             <div className="flex flex-col mt-4">
               <div className="flex items-center justify-between">
-                <p>Correctly Answered: {correctCount}</p>
                 <div className="bg-[#1B9D34] w-3 h-3 ml-2"></div>
+                <div className=" font-medium md:text-lg text-sm  ">
+                  Correctly Answered
+                </div>
+                <div className=" font-medium md:text-lg text-sm  ">
+                  {correctCount}
+                </div>
               </div>
-              <hr className="w-full my-4 border-t-2 border-gray-400" />
+              <hr className="w-full my-4 border-t-2 border-[#414142]" />
               <div className="flex items-center justify-between">
-                <p>Total Questions: {totalQuestions}</p>
                 <div className="bg-[#777777] w-3 h-3 ml-2"></div>
+
+                <div className=" font-medium md:text-lg text-sm ">
+                  Total Questions
+                </div>
+                <div className=" font-medium md:text-lg text-sm  ">
+                  {" "}
+                  {totalQuestions}
+                </div>
               </div>
             </div>
           </div>
         </div>
 
         <button
-          className="mt-8 bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
+          className="mt-5 bg-indigo-500 hover:bg-indigo-700 text-white font-bold md:p-4 p-2 rounded"
           onClick={handleBackToHome}
         >
           Back to Home
         </button>
       </div>
 
-      <div className="w-[90%] h-[10%] bg-white"></div>
+      <div className="w-[90%] md:h-14 h-10 mb-3 mt-2 rounded-3xl bg-white"></div>
     </div>
   );
 };
